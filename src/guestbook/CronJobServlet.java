5803cab1dfafb5f36e2c345f5bce52277533cb6d
Tfermis <etjm03@utexas.edu>
2016-02-18 21:39:05 -0600

    cron job initial commit

--- src/guestbook/CronJobServlet.java
+++ src/guestbook/CronJobServlet.java
@@ -6,19 +6,44 @@ import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.*;
 
+import java.util.Properties;
+import javax.mail.Message;
+import javax.mail.MessagingException;
+import javax.mail.Session;
+import javax.mail.Transport;
+import javax.mail.internet.AddressException;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeMessage;
+
+
+
 @SuppressWarnings("serial")
 public class CronJobServlet extends HttpServlet 
 {
 	private static final Logger _logger = Logger.getLogger(CronJobServlet.class.getName());
 	public void doGet(HttpServletRequest req, HttpServletResponse resp)
 	throws IOException {
-	
-		try 
-		{
-		_logger.info("Cron Job has been executed");
+		Properties props = new Properties();
+		Session session = Session.getDefaultInstance(props, null);
+		String msgBody = "...";
 		
-		//Put your logic here
-
+		try {
+			//in here, do a for loop with all the subscription email addresses.
+			
+			
+			_logger.info("Cron Job has been executed");
+		    Message msg = new MimeMessage(session);
+		    msg.setFrom(new InternetAddress("admin@example.com", "Example.com Admin"));
+		    msg.addRecipient(Message.RecipientType.TO,
+		     new InternetAddress("user@example.com", "Mr. User"));
+		    msg.setSubject("Your Example.com account has been activated");
+		    msg.setText(msgBody);
+		    Transport.send(msg);
+		
+		} catch (AddressException e) {
+		    // ...
+		} catch (MessagingException e) {
+		    // ...
 		}
 		catch (Exception ex) 
 		{
